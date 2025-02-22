3bc9634a1c4fff5ab5954408bf959f5bd3fa7df4
SergiuTigan <sergiu@tigan.dev>
2025-02-22 08:00:21 +0200

    Navbar and Footer

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [
+    RouterLink
+  ],
+  standalone: true,
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+
+}
