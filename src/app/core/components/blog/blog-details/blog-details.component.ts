c188a5ec98b4765632015e817ab64cb15e7b864f
SergiuTigan <sergiu@tigan.dev>
2025-02-26 05:55:21 +0200

    Created Blog Details Component

---
+++ src/app/core/components/blog/blog-details/blog-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-details',
+  imports: [],
+  standalone: true,
+  templateUrl: './blog-details.component.html',
+  styleUrl: './blog-details.component.scss'
+})
+export class BlogDetailsComponent {
+
+}
--- src/app/core/components/blog/blog-routing.module.ts
+++ src/app/core/components/blog/blog-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogComponent } from './blog.component';
+import { BlogDetailsComponent } from './blog-details/blog-details.component';
 
 const blogRoutes: Routes = [
-  { path: '', component: BlogComponent }
+  { path: '', component: BlogComponent,
+    children: [
+      {path: ':id', component: BlogDetailsComponent}
+    ]}
 ];
 
 @NgModule({
