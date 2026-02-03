2c7803e87df971b04e15160863ab097b06fc0468
Sergiu Tigan <sergiutigan@gmail.com>
2026-02-03 15:30:15 +0200

    fix: remove blog/profile routes, update project details for dark mode
    - Removed blog and profile routes (not needed for portfolio)
    - Updated project details page with dark mode styling
    - Simplified routing to only: home, projects, about, contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,33 +8,6 @@ export const routes: Route[] = [
     data: {animation: 'HomePage'}
   },
   {
-    path: 'blog',
-    loadComponent: () => import('./core/components/blog/blog.component').then(m => m.BlogComponent),
-    data: {animation: 'BlogPage'},
-    children: [
-      {
-        path: '',
-        loadComponent: () => import('./core/components/blog/blog-grid/blog-grid.component').then(m => m.BlogGridComponent)
-      },
-      {
-        path: 'details/:id',
-        loadComponent: () => import('./core/components/blog/blog-details/blog-details.component').then(m => m.BlogDetailsComponent)
-      },
-      {
-        path: 'create',
-        loadComponent: () => import('./core/components/blog/blog-create/blog-create.component').then(m => m.BlogCreateComponent)
-      },
-      {
-        path: 'edit/:id',
-        loadComponent: () => import('./core/components/blog/blog-create/blog-create.component').then(m => m.BlogCreateComponent)
-      },
-      {
-        path: 'tags/:tag',
-        loadComponent: () => import('./core/components/blog/blog-grid/blog-grid.component').then(m => m.BlogGridComponent)
-      }
-    ],
-  },
-  {
     path: 'projects',
     loadComponent: () => import('./core/components/projects/projects.component').then(m => m.ProjectsComponent),
     data: {animation: 'ProjectsPage'},
@@ -42,17 +15,10 @@ export const routes: Route[] = [
       {
         path: '',
         loadComponent: () => import('./core/components/projects/projects-grid/projects-grid.component').then(m => m.ProjectsGridComponent)
-      }, {
+      },
+      {
         path: 'details/:id',
         loadComponent: () => import('./core/components/projects/projects-details/projects-details.component').then(m => m.ProjectsDetailsComponent)
-      },
-      {
-        path: 'create',
-        loadComponent: () => import('./core/components/projects/projects-create/projects-create.component').then(m => m.ProjectsCreateComponent)
-      },
-      {
-        path: 'edit/:id',
-        loadComponent: () => import('./core/components/projects/projects-create/projects-create.component').then(m => m.ProjectsCreateComponent)
       }
     ]
   },
@@ -65,11 +31,6 @@ export const routes: Route[] = [
     path: 'contact',
     loadComponent: () => import('./core/components/contact/contact.component').then(m => m.ContactComponent),
     data: {animation: 'ContactPage'}
-  },
-  {
-    path: 'profile',
-    loadComponent: () => import('./core/components/profile/profile.component').then(m => m.ProfileComponent),
-    data: {animation: 'ProfilePage'}
   },
   {path: '**', redirectTo: 'home'}
 ];
